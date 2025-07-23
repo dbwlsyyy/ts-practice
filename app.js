defd07695972bd8efacceeb37fc7a4bf40ad2e6b
홍유진 <dbwlsyyy@naver.com>
2025-07-24 01:51:28 +0900

    ts 개념연습1: 오류 수정

--- app.js
+++ app.js
@@ -9,26 +9,32 @@ class UserManager {
     constructor(initialUsers = []) {
         this._users = [];
         this.addUser = (user) => {
-            this._users.map((u) => u.email === user.email
-                ? console.log('email 중복, 유저 추가 실패')
-                : (() => {
-                    this._users = [...this._users, user];
-                    console.log('유저 추가 성공');
-                })());
+            const emailExist = this._users.some((existingUser) => existingUser.email === user.email);
+            if (emailExist) {
+                console.log('이메일 중복, 유저 추가 실패');
+                return false;
+            }
+            this._users.push(user);
+            console.log('유저 추가 성공');
+            return true;
         };
-        // side effect를 위해 map을 사용하면 안됨. map은 각 요소에 대해 함수를 실행하고 새로운 배열을 반환하는 함수임
-        //
+        // side effect를 위해 map을 사용하면 안됨.
+        // map은 각 요소에 대해 함수를 실행하고 새로운 배열을 반환하는 함수임
         this.getUserById = (id) => {
             const filteredUser = this._users.find((u) => u.id === id);
             return filteredUser !== null && filteredUser !== void 0 ? filteredUser : null;
         };
         this.updateUserRole = (id, newRole) => {
-            this._users.map((u) => u.id === id
-                ? (() => {
-                    u.role = newRole;
-                    console.log(`유저 ${id}의 role 업데이트 완료`);
-                })()
-                : console.log('에러 : 해당 사용자가 없습니다.')); // 여기서 질문 ! 타입 가드 없이 이렇게 해도되지 않아 ? 이거 대신 타입 가드 하려면 좀 복잡해지는 것 같은데
+            const userToUpdate = this.getUserById(id);
+            if (typeof userToUpdate !== null) {
+                userToUpdate.role = newRole;
+                console.log('해당 아이디는 없는 아이디 입니다, update 실패');
+                return false;
+            }
+            else {
+                console.log(`${id}번 아이디의 유저 role이 업데이트 됨, update 성공`);
+                return true;
+            }
         };
         this._users = initialUsers;
     }
--- app.ts
+++ app.ts
@@ -23,32 +23,41 @@ class UserManager {
         return [...this._users];
     }
 
-    public addUser = (user: User) => {
-        this._users.map((u) =>
-            u.email === user.email
-                ? console.log('email 중복, 유저 추가 실패')
-                : (() => {
-                      this._users = [...this._users, user];
-                      console.log('유저 추가 성공');
-                  })()
+    public addUser = (user: User): Boolean => {
+        const emailExist = this._users.some(
+            (existingUser) => existingUser.email === user.email
         );
+
+        if (emailExist) {
+            console.log('이메일 중복, 유저 추가 실패');
+            return false;
+        }
+
+        this._users.push(user);
+        console.log('유저 추가 성공');
+        return true;
     };
-    // side effect를 위해 map을 사용하면 안됨. map은 각 요소에 대해 함수를 실행하고 새로운 배열을 반환하는 함수임
-    //
+    // side effect를 위해 map을 사용하면 안됨.
+    // map은 각 요소에 대해 함수를 실행하고 새로운 배열을 반환하는 함수임
 
     public getUserById = (id: number): User | null => {
         const filteredUser = this._users.find((u) => u.id === id);
         return filteredUser ?? null;
     };
 
-    public updateUserRole = (id: number, newRole: UserRole) => {
-        this._users.map((u) =>
-            u.id === id
-                ? (() => {
-                      u.role = newRole;
-                      console.log(`유저 ${id}의 role 업데이트 완료`);
-                  })()
-                : console.log('에러 : 해당 사용자가 없습니다.')
-        ); // 여기서 질문 ! 타입 가드 없이 이렇게 해도되지 않아 ? 이거 대신 타입 가드 하려면 좀 복잡해지는 것 같은데
+    public updateUserRole = (id: number, newRole: UserRole): Boolean => {
+        const userToUpdate = this.getUserById(id);
+
+        if (typeof userToUpdate !== null) {
+            userToUpdate.role = newRole;
+
+            console.log('해당 아이디는 없는 아이디 입니다, update 실패');
+            return false;
+        } else {
+            console.log(
+                `${id}번 아이디의 유저 role이 업데이트 됨, update 성공`
+            );
+            return true;
+        }
     };
 }
